87edc18909c5ca1d664bf718d7c5234c5d219342
Pablo Garcia <pgarciag566@gmail.com>
2022-07-24 00:16:12 -0500

    moved and configured some things from models

--- models/User.js
+++ models/User.js
@@ -1,4 +1,61 @@
-const User = require("./User");
-const Thought = require("./Thought");
+const {Schema, model } = require("mongoose");
 
-module.exports = { User, Thought };
+
+var validateEmail = function(email) {
+    var re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email)
+};
+
+//Schema to create User model
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+            validate: [validateEmail, "Please enter a valid email"],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please fill a valid email address",
+            ],
+        },
+        
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "thought"
+            }
+        ],
+
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "user"
+            }
+        ]
+
+    },
+
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
+    }
+);
+
+//Virtual Shema retrieves the length of users friends
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+});
+
+const User = model("user", userSchema);
+
+module.exports = User;
