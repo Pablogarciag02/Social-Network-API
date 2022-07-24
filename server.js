ebab91cebed2e9ca47b244cd3cf405f426c177cc
Pablo Garcia <pgarciag566@gmail.com>
2022-07-24 00:14:09 -0500

    server now executes correctly

--- server.js
+++ server.js
@@ -7,17 +7,15 @@ const cwd = process.cwd();
 const PORT = 3001;
 const app = express();
 
-//Not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
-const activity = cwd.includes('Social-Network-API')
-  ? cwd.split('/Social-Network-Api')[1]
-  : cwd;
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(routes);
 
 db.once("open", () => {
-    app.listen(PORT, () => {
-        console.log(`API server for${activity} running on port ${PORT}!`)
-    })
-})
+  app.listen(PORT, () => {
+      console.log(`Social Network API running on port ${PORT}!`)
+  })
+});
+
+const router = require("express").Router();
+
